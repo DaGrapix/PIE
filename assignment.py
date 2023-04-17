acf21f4eb1a6a3324e925c2535d8e64bea3c973e
Florine Lefer <florine.lefer@ensta-paris.fr>
2023-04-17 15:56:48 +0200

    Ajout option pour zone en anneau + affichage

--- assignment.py
+++ assignment.py
@@ -1,21 +1,82 @@
 import game
 import math
-from shapely.geometry import Point, Polygon
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from shapely.geometry import Polygon, Point, LineString
 
-# Compute the distance between a drone and a zone
-# Return : the distance
+
+
+
+def divide_ring_zone(center, inner_radius, outer_radius, nb_zones):
+    '''Divides a ring zone into multiple sub zones
+    
+    Args:
+         center (tuple): center of the ring
+         inner_radius (float): radius of the inner circle of the ring
+         outer_radius (float): radius of the outer cirlce of the ring
+         nb_zones (int)
+    Returns:
+         sub_zones (3 dimension float list): list of sub zones outline coordinates
+    '''
+    center = Point(center[0], center[1])
+    inner_circle = center.buffer(inner_radius)
+    outer_circle = center.buffer(outer_radius)
+
+    # Divide inner and outer circle in nb_zones near equal parts
+    sub_arrays_inner = np.array_split(inner_circle.exterior.coords, nb_zones)
+    sub_arrays_outer = np.array_split(outer_circle.exterior.coords, nb_zones)
+    # Add connection points to sub arrays 
+    for i in range(nb_zones) :
+        sub_arrays_inner[i] = np.concatenate((sub_arrays_inner[i], [sub_arrays_inner[(i+1)%nb_zones][0]]), axis=0)
+        sub_arrays_outer[i] = np.concatenate((sub_arrays_outer[i], [sub_arrays_outer[(i+1)%nb_zones][0]]), axis=0)
+        
+    # Sub zones creation
+    sub_zones = []
+    for i in range(nb_zones):
+        # Concatenate outer circle part with the corresponding inner circle part to create the sub zone
+        concat = np.concatenate((np.flip(sub_arrays_inner[i], axis=0), sub_arrays_outer[i]))
+        sub_zone = Polygon(concat)
+        # Coords of sub zone
+        zone_coords = []
+        for coord in sub_zone.exterior.coords:
+            zone_coords.append([coord[0], coord[1]])
+        sub_zones.append(zone_coords)
+    return sub_zones
+
+
+
+
 def computeDistance(drone, zone) :
+    '''Computes the distance between a drone and a zone
+
+    Args:
+          drone (Drone): a drone
+          zone (2 dimension float list): list of zone outline coordinates
+    
+    Returns:
+          rect.distance(point) (float): min distance between drone and zone
+    '''
     rect = Polygon(zone)
     drone_pos = (drone.position.x, drone.position.y)
     point = Point(drone_pos)
     return rect.distance(point)
 
 
-# Assigns each drone to a zone
-# Return :  - assignment_list : a list of couples (drone, zone assigned)
-#           - list(set(drones_list) - set(assigned_drones)) : left drones (if number of drones > number of zones)
-#           - zones_list : left zones (if number of zones > number of drones)
+
+
 def assignment(drones_list, zones_list) :
+    '''Assigns each drone to a zone
+
+    Args:
+          drones_list (Drone list): list of drones
+          zones_list (3 dimension float list): list of zones outline coordinates
+
+    Returns:
+          assignment_list (tuple (Drone, 2 dimension float list)): list of couples (drone, zone assigned)
+          list(set(drones_list) - set(assigned_drones)) (2 dimension Drone list): list of unassigned drones (if number of drones > number of zones)
+          zones_list (3 dimension float list): list of unassigned zones outline coordinates (if number of zones > number of drones)
+    '''
     if not drones_list or not zones_list :
         print("No drone or no zone for assignment")
     assignment_list = []
@@ -31,68 +92,64 @@ def assignment(drones_list, zones_list) :
                     zone_min = zone
             zones_list.remove(zone_min)
             assigned_drones.append(drone)
-            print("Assignment list append :", drone.position, ", ", zone_min)
             assignment_list.append((drone, zone_min))
     return assignment_list, list(set(drones_list) - set(assigned_drones)), zones_list
 
+
+
+
+def print_assignment(assignment_list, left_drones_list, left_zones_list) :
+    '''Prints the result of the assignement
+
+    Args:
+         assignment_list (tuple (Drone, 2 dimension list)): list of couples (drone, zone assigned)
+         left_drones_list (2 dimension float list): list of unassigned drones (if number of drones > number of zones)
+         left_zones_list (3 dimension float list): list of unassigned zones outline coordinates (if number of zones > number of drones)
+
+    Returns:
+    '''
+    no_of_colors = len(assignment_list)
+    color = ["#"+''.join([random.choice('123456789ABCDEF') for i in range(6)]) for j in range(no_of_colors)]
+    for i, (drone, zone) in enumerate(assignment_list) :
+        print(i)
+        plt.plot(drone.position.x, drone.position.y, marker ='1', c=color[i])
+        x_coords = []
+        y_coords = []
+        for (x, y) in zone :
+            x_coords.append(x)
+            y_coords.append(y)
+        plt.plot(x_coords, y_coords, c=color[i])
+    for drone in left_drones_list :
+        plt.scatter(drone.position.x, drone.position.y, marker='1', c='black')
+    for zone in left_zones_list :
+        x_coords = []
+        y_coords = []
+        for (x, y) in zone :
+            x_coords.append(x)
+            y_coords.append(y)
+        plt.plot(x_coords, y_coords, c='black')
+    plt.show()
+
+
+    
+
 def main() :
+    
     drone_1 = game.Drone(0, 0)
-    drone_2 = game.Drone(100, 100)
+    drone_2 = game.Drone(10, 10)
+    drone_3 = game.Drone(0, 20)
     zone_1 = [[2, 4], [3, 4], [2, 7], [3, 7]]
     zone_2 = [[125, 125], [190, 125], [125, 300], [125, 300]]
     zone_3 = [[300, 300], [400, 300], [300, 500], [300, 500]]
-    a, b, c  = assignment([drone_1, drone_2], [zone_2])
-    print("\nAssignment list :", a,"\nLeft drones :", b,"\nLeft zones", c)
+    zone_list = divide_ring_zone((0.,0.), 3., 5., 2)
+    a, b, c  = assignment([drone_1, drone_2, drone_3], zone_list)
+    #print("\nAssignment list :", a,"\nLeft drones :", b,"\nLeft zones", c)
+    print_assignment(a, b, c)
+    
+    
     
 if __name__ == '__main__':
     main()
 
 
 
-
-
-'''
-# Importer les modules nécessaires
-import math
-
-# Définir la fonction first_last_waypoint
-def first_last_waypoint(trajectories_left, current_position):
-    start = current_position
-    end = trajectories_left[0][-1]
-    total_path_length = math.dist(start, end)
-    return [start, 
-            total_path_length]
-
-# Initialiser les variables
-trajectories_num = len(trajectories_total)
-trajectories_assigned = []
-trajectories_left = [i for i in range(1, trajectories_num + 1)]
-
-total_path_length = []
-current_positions = [0 for i in range(n_drones)]
-
-# Boucle principale
-while trajectories_assigned != trajectories_total:
-    # Parcourir tous les drones
-    for i in range(n_drones):
-        # Vérifier si toutes les trajectoires ont été assignées
-        if not trajectories_left:
-            break
-        # Trouver la trajectoire avec la longueur la plus courte
-        min_length = min(total_path_length)
-        min_index = total_path_length.index(min_length)
-        # Assigner la trajectoire au drone correspondant
-        drone_index = min_index % n_drones
-        trajectory_index = trajectories_left.pop(min_index // n_drones)
-        trajectories_assigned.append(trajectory_index)
-        current_positions[drone_index] = trajectories_total[trajectory_index - 1][-1]
-        # Mettre à jour les longueurs de chemin
-        total_path_length.pop(min_index)
-        for j in range(len(trajectories_left)):
-            start = current_positions[drone_index]
-            end = trajectories_total[trajectories_left[j] - 1][-1]
-            total_path_length[j] = first_last_waypoint([trajectories_left[j]], start)[1] + math.dist(start, end)
-    # Construire ou mettre à jour les trajectoires finales
-    # Mettre à jour les positions actuelles des drones
-    # Mettre à jour les trajectoires restantes
-'''
